f68d7f4606d6c7d4e36ead42eddb6a7c016c7906
zj <819685619@qq.com>
2023-03-04 17:15:28 +0800

    add chunk, disassemble, opcode

---
+++ src/chunk.h
@@ -0,0 +1,30 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+  OP_CONSTANT,
+  OP_RETURN,
+} OpCode;
+
+typedef struct {
+  uint8_t* code;
+  int count;
+  int capacity;
+  ValueArray constants;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
+
+int addConstant(Chunk* chunk, Value value);
+
+
+
+
+
+
+#endif
