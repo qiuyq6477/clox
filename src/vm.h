e3f498cf66dde9e29f97dc09868a8a2e5141a2a6
zj <819685619@qq.com>
2023-03-09 15:36:39 +0800

    add hash map and opt string equal test

--- src/vm.h
+++ src/vm.h
@@ -3,6 +3,7 @@
 
 #include "chunk.h"
 #include "value.h"
+#include "table.h"
 
 #define STACK_MAX 256
 
@@ -12,6 +13,7 @@ typedef struct {
   Value stack[STACK_MAX];
   Value* stackTop; //指针指向数组中栈顶元素的下一个元素位置
   Obj* objects;
+  Table strings;
 } VM;
 
 typedef enum {
